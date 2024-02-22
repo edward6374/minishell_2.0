c2a8710a7106a9dfaf0e3176ded0eb7ea7ebbcf1
Meritxell <meriusky@Ordenador-portatil-de-Meritxell.local>
2024-02-22 13:17:39 +0100

    trying to not do a caos and clean the main

--- test.c
+++ test.c
@@ -3,6 +3,19 @@
 #include "libft/libft.h"
 #include "readline/readline.h"
 #include "readline/history.h"
+/*hay que intentar simplemnte coger la estring y printearla por las palabritas separadas por pipes.
+Si se puede hacer eso, solo quedara hacer una fucion como la ft_lstadd_back_bonus.c del libft
+para separar las cosas. O algo rollo esto:
+
+// Print the contents of the list
+    printf("Contents of the list:\n");
+    t_test *temp = list;
+    while (temp != NULL)
+    {
+        printf("%s\n", temp->str);
+        temp = temp->next;
+    }
+para meter las cosas en la lista*/
 
 typedef struct s_test
 {
@@ -11,24 +24,38 @@ typedef struct s_test
     struct s_test *prev;
     struct s_test *next;
 } t_test;
+/*
+int	count_pipe(char *input)
+{
+	int count;
+	int pipe;
 
+	count = 0;
+	pipe = 0;
+	while(input[count])
+	{
+		if(input[count] == '|')
+			pipe++;
+		count++;
+	}
+	return(pipe);
+}*/
 int main()
 {
 	char	*input;
-	t_test	*list;
-	t_test	*new;
+	//t_test	*list;
+	//t_test	*new;
 	int		i;
 	int 	j;
 	int		count_for_malloc;
 	char	*div_str;
-	int 	controll_readline;
+//	int 	controll_readline;
 
-	list = NULL;
+//	list = NULL;
 	i = 0;
 	div_str = NULL;
 	count_for_malloc = 0;
-	controll_readline = 0;
-	// Loop to continuously read input until "exit" is entered
+//	controll_readline = 0;
 	while(1)
 	{
 		input = readline("Enter a command: ");
@@ -42,9 +69,6 @@ int main()
 			free(input);
 			continue;
 		}
-	//	list = NULL;// esto en verdad no se si hace algo que este aqui abajo(antes estaba declarado arriba)
-	//	new = NULL;
-	//	free(new->str);
 		if (ft_strcmp(input, "exit") == 0)//check if user wnts to exit
 		{
 			printf("if del exit\n");
@@ -58,7 +82,7 @@ int main()
 			count_for_malloc = i;
 			while(input[count_for_malloc] != '|' && input[count_for_malloc] != '\0')//to reservate memory with malloc
 				count_for_malloc++;
-			free(div_str);
+			//free(div_str);
 			div_str = malloc((count_for_malloc - i + 1) * (sizeof(char)));
 			j = 0;
 			while(input[i] != '|' && input[i] != '\0')//to safe every comand on strinig (not necessery to controlle space
@@ -68,7 +92,10 @@ int main()
 				i++;
 				j++;
 			}
-			
+			printf("%s\n", div_str);
+			free(div_str);
+			i++;
+		/*	
 			if(list == NULL)
 			{
 				new = ft_calloc(1, sizeof(t_test));
@@ -78,10 +105,8 @@ int main()
 				list = new;
 				new->prev = NULL;
 				i++;
-			//	new = ft_calloc(1, sizeof(t_test));
 				continue;
 			}
-		//	printf("cuanto es pos: %d\n", new->pos);
 			if(list != NULL && (input[i] == '|' || input[i] == '\0'))
 			{
 				new = ft_calloc(1, sizeof(t_test));
@@ -90,7 +115,6 @@ int main()
 				printf("segundo metido en new: %s\n", div_str);
 				list->next = new;
 				new->prev = list;
-			//	new = ft_calloc(1, sizeof(t_test));
 				i++;
 				continue;
 			}
@@ -111,7 +135,7 @@ int main()
 		//		free(div);
 		//		free(input);
 		//		break;
-		//	}
+		//	}*/
 		free(input);
 		}
 	}
