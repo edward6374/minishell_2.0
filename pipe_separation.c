2e63223bfc9fce657d9b577e97d478352c1032f9
Meritxell Hernandez morraja <mehernan@car12s2.42barcelona.com>
2024-03-07 21:13:10 +0100

    ¬¬¬¬
    '

--- pipe_separation.c
+++ pipe_separation.c
@@ -7,6 +7,11 @@
 Si se puede hacer eso, solo quedara hacer una fucion como la ft_lstadd_back_bonus.c del libft
 para separar las cosas. O algo rollo esto:
 
+Idea de lo que hay que hacer: Se complica la cosa cn el prev y el next de la lista. Lo que he
+pensado es pasar todo lo que hay de listas incluida la estructura a otro archivo. Ahi tendre
+que (con el pos) ordenar, si hay solo 1pos no habra ni next ni prev si hay mas si. Ademas que
+el pos lo puedes sacar de la estructura y simplemente hazerlo variable normal.
+
 // Print the contents of the list
     printf("Contents of the list:\n");
     t_test *temp = list;
@@ -16,7 +21,48 @@ para separar las cosas. O algo rollo esto:
         temp = temp->next;
     }
 para meter las cosas en la lista*/
+typedef struct s_test
+{
+    int pos;
+    char *str;
+    struct s_test *prev;
+    struct s_test *next;
+} t_test;
 
+void	*put_list(char *div_str)//a ver
+{
+    t_test	*list;
+	t_test	*new;
+	t_test	pos;
+
+	list = NULL;
+	pos = .
+	if(div_str!)
+		break;
+	while(pos !< 1)
+	{
+		list->str = div_str
+	}
+/*    while (temp != NULL)
+    {
+        printf("%s\n", temp->str);
+        temp = temp->next;
+    }*/
+}
+void	pos_num(char *input)//to know how many positions will be
+{
+	int i;
+	t_test pos;
+	
+	pos = 1;
+	i = 0;
+	while(input[i]!)
+	{
+		if(input[i] == '|')
+			pos++;
+		i++;
+	}
+}
 int main()
 {
 	char	*input;
@@ -50,6 +96,7 @@ int main()
 		add_history(input);
 		//while para coger cada palabra despues del pipe
 		i = 0;
+		pos_num(input);
 		while (input[i])
 		{
 			count_for_malloc = i;
@@ -66,6 +113,7 @@ int main()
 				j++;
 			}
 			printf("%s\n", div_str);
+// NO		put_list(div_strs);// here we should send the string to the function that will allocateit in the list
 			free(div_str);
 			if(input[i] != '\0')
 				i++;
